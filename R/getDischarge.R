a7968c0a8914219ca7392e79935137cc4841b44c
wdwatkins <wwatkins@usgs.gov>
2016-06-07 15:58:07 -0500

    Added a few more function arguments
    Reinstated the retry function, was getting 502 bad gateway errors

--- R/getDischarge.R
+++ R/getDischarge.R
@@ -3,7 +3,33 @@
 library(dataRetrieval)
 library(dplyr)
 
-getDischarge <- function(bbox = "-85,29,-80,33.9"){
+retryNWIS <- function(..., retries=3){
+  
+  safeWQP = function(...){
+    result = tryCatch({
+      readNWISdata(...)
+    }, error = function(e) {
+      if(e$message == 'Operation was aborted by an application callback'){
+        stop(e)
+      }
+      return(NULL)
+    })
+    return(result)
+  }
+  retry = 1
+  while (retry < retries){
+    result = safeWQP(...)
+    if (!is.null(result)){
+      retry = retries
+    } else {
+      message('query failed, retrying')
+      retry = retry+1
+    }
+  }
+  return(result)
+}
+
+getDischarge <- function(bbox = "-85,29,-80,33.9", minDrainage = "100", start="2016-06-4", end="2016-06-06"){
   
   #find sites within lat/lon box, >100 km^2 drainage
   #expanded output option didnt seem to provide anything extra?
@@ -11,8 +37,8 @@ getDischarge <- function(bbox = "-85,29,-80,33.9"){
   
   #get the actual data, converted to numeric format
   siteNos <- sites$site_no
-  startDate <- as.Date("2016-06-4")
-  endDate <- as.Date("2016-06-05")
+  startDate <- as.Date(start)
+  endDate <- as.Date(end)
   
   #loop over sites, get data, concatenate
   #leave out sites with no data or sample rates > 15 min (192 samples/2 days)
@@ -21,8 +47,8 @@ getDischarge <- function(bbox = "-85,29,-80,33.9"){
   
   for (site in siteNos)
   {
-    siteQ <- readNWISdata(service = "iv",convertType = TRUE, sites=site, 
-                          startDate=startDate, endDate=endDate, parameterCd="00060")
+    siteQ <- retryNWIS(service = "iv",convertType = TRUE, sites=site, 
+                       startDate=startDate, endDate=endDate, parameterCd="00060")
     #print(dim(siteQ))
     siteQ <- renameNWISColumns(siteQ)
     
@@ -39,8 +65,8 @@ getDischarge <- function(bbox = "-85,29,-80,33.9"){
       sampRate = siteQ$dateTime[2] - siteQ$dateTime[1] #check that the sample rate is every 15 min
       if ( sampRate == 15)
       {
-        print(site)
-        print(dim(siteQ))
+        #print(site)
+        #print(dim(siteQ))
         allQ <- rbind(allQ,siteQ)
       }
     }
@@ -51,3 +77,5 @@ getDischarge <- function(bbox = "-85,29,-80,33.9"){
   
   return(allQ_info)
 }
+
+#Q <- getDischarge() #test the function
